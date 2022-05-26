5c5573e809762c34caa67bb488ecefb06d027126
Yeonjeong Shin <87056545+shinyj1@users.noreply.github.com>
2022-05-27 06:44:31 +0900

    modify ejs and js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const express = require("express"),
   app = express(),
   homeController = require("./controllers/homeController"),
@@ -8,7 +6,6 @@ const express = require("express"),
 	path = require('path');
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
 app.set("port", process.env.PORT || 80);
 app.use(
   express.urlencoded({
@@ -19,43 +16,13 @@ app.use(express.json());
 app.use(layouts);
 app.use(express.static("public"));
 
-app.set('layout', 'layout');
-app.set("layout extractScripts", true);
-
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-/*app.get("/schedule1", (req, res) => {
-	res.render("schedule1");
-});
-
-app.get("/schedule2", (req, res) => {
-	res.render("schedule2");
-});*/
-
 app.get("/schedule1", homeController.showscdl1);
 app.get("/schedule2", homeController.showscdl2);
-//app.post("/contact", homeController.postedSignUpForm);
 
-/*const mysql = require('mysql2/promise');
-let test = async () => {
-	const db = mysql.createPool({
-		host: '34.64.52.97',
-		user: 'cc',
-		password: 'password',
-		port: 3306,
-		database: 'test_db',
-		waitForConnections: true,
-		insecureAuth: true
-	});
-	let sql = 'SELECT * FROM member';
-	let [rows, fields] = await db.query(sql);
-	//console.log(rows);
-	
-	
-};
-test();*/
 app.get("/test", homeController.test);
 
 app.use(errorController.pageNotFoundError);
