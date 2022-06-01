f651dcc370d631ea338ad0381185cf68a89a81d6
Yeonjeong Shin <87056545+shinyj1@users.noreply.github.com>
2022-06-01 22:17:46 +0900

    addSchedule - sql 수정
    일정 추가할 때 scdlMemId, scdlPtId 직접 값 입력 안하고 parttime에서 받아오게 수정

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,6 +1,8 @@
 const db = require("../dbconnection"),
 getScheduleParams = body => {
     return {
+        scdlMemId : body.scdlMemId,
+        scdlPtId : body.scdlPtId,
         wage : body.wage,
         startTime : body.startTime,
         endTime : body.endTime,
@@ -63,7 +65,7 @@ module.exports = {
         try {
             let scdl = getScheduleParams(req.body);
             let sql = "INSERT INTO schedule(scdlMemId, scdlPtId, isCovered, startTime, endTime, holiday, overPay, rest, night, extra, wage) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            let params = [2, 4, scdl.isCovered, scdl.startTime, scdl.endTime, scdl.holiday, scdl.overPay, scdl.rest, scdl.night, scdl.extra, scdl.wage];
+            let params = [scdl.scdlMemId, scdl.scdlPtId, scdl.isCovered, scdl.startTime, scdl.endTime, scdl.holiday, scdl.overPay, scdl.rest, scdl.night, scdl.extra, scdl.wage];
             await db.query(sql, params);
             res.render("submit");
         } catch (err) {
